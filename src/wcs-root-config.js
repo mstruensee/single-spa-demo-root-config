6d3d67c9415e02a6f187372643f87c0f22c2a78b
Matthew Struensee <mstruensee@gmail.com>
2021-07-28 13:46:00 -0500

    fixed

--- src/wcs-root-config.js
+++ src/wcs-root-config.js
@@ -5,18 +5,18 @@ registerApplication(
   "@wcs/single-spa-demo-nav",
   () => System.import("@wcs/single-spa-demo-nav"),
   isActive.nav,
-  { domElement: document.getElementById('nav-container') }
+  { domElement: document.getElementById("nav-container") }
 );
 registerApplication(
   "@wcs/single-spa-demo-page-1",
   () => System.import("@wcs/single-spa-demo-page-1"),
   isActive.page1,
-  { domElement: document.getElementById('page-1-container') }
+  { domElement: document.getElementById("page-1-container") }
 );
 registerApplication(
   "@wcs/single-spa-demo-page-2",
   () => System.import("@wcs/single-spa-demo-page-2"),
   isActive.page2,
-  { domElement: document.getElementById('page-2-container') }
+  { domElement: document.getElementById("page-2-container") }
 );
 start();
