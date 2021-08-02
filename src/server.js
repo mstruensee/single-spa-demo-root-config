7eeb0660eed5906165e084b28612d176910f546c
Matthew Struensee <mstruensee@gmail.com>
2021-08-02 13:54:32 -0500

    clean up

--- src/server.js
+++ src/server.js
@@ -3,12 +3,19 @@
 const express = require("express");
 const path = require("path");
 const serverless = require("serverless-http");
-//const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => res.sendFile("index.html", { root: "dist" }));
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+if (process.env.isLocal) {
+  const PORT = process.env.PORT || 5000;
+  /* eslint-disable-next-line */
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+}
 
 const handler = new serverless(app);
-module.exports = { handler };
+module.exports = {
+  handler,
+  app,
+};
--- src/wcs-root-config.js
+++ src/wcs-root-config.js
@@ -1,22 +1,28 @@
 import { registerApplication, start } from "single-spa";
-import * as isActive from "./activity-functions";
+
+const prefix = (location, ...prefixes) =>
+  prefixes.some(
+    (prefix) => location.href.indexOf(`${location.origin}/${prefix}`) !== -1
+  );
+
+const getElementById = (id) => document.getElementById(id);
 
 registerApplication(
   "@wcs/single-spa-demo-nav",
   () => System.import("@wcs/single-spa-demo-nav"),
-  isActive.nav,
-  { domElement: document.getElementById("nav-container") }
+  () => true,
+  { domElement: getElementById("nav-container") }
 );
 registerApplication(
   "@wcs/single-spa-demo-page-1",
   () => System.import("@wcs/single-spa-demo-page-1"),
-  isActive.page1,
-  { domElement: document.getElementById("page-1-container") }
+  (location) => prefix(location, "page1"),
+  { domElement: getElementById("page-1-container") }
 );
 registerApplication(
   "@wcs/single-spa-demo-page-2",
   () => System.import("@wcs/single-spa-demo-page-2"),
-  isActive.page2,
-  { domElement: document.getElementById("page-2-container") }
+  (location) => prefix(location, "page2"),
+  { domElement: getElementById("page-2-container") }
 );
 start();
